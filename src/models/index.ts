c2f020aab1b504a3bcfce58dbed8bc42728efddc
Diego Reyes <diegorele13@gmail.com>
2025-08-11 12:04:32 -0500

    feat(models): add DocumentAnalysis to exported models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,11 @@
 import { User } from './user.model'
 import { Workspace } from './workspace.model'
+import { DocumentAnalysis } from './analysis.model'
 
 const models = {
   User,
-  Workspace
+  Workspace,
+  DocumentAnalysis
 }
 
 export default models;
