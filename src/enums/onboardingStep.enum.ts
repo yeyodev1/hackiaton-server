ea1330a2a607080bd0c9a665f58256e5c4189825
Diego Reyes <diegorele13@gmail.com>
2025-08-11 12:03:48 -0500

    refactor: remove unused OnboardingStepEnum

--- src/enums/onboardingStep.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum OnboardingStepEnum {
-  PENDING_DATA_SUBMISSION = "pending_data_submission",
-  PENDING_MEETING_SCHEDULE = "pending_meeting_schedule",
-  MEETING_SCHEDULED = "meeting_scheduled",
-  ONBOARDING_COMPLETE = "onboarding_complete",
-}
